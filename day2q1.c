c5bd5d62d57c77157ffc144f24d9ed6b67acdbad
aadi20081 <aadianandjkps@gmail.com>
2026-09-07 11:40:32 +0530

    Add files via upload

---
+++ day2q1.c
@@ -0,0 +1,16 @@
+//Write a program to input length and breadth of a rectangle and display its area and perimeter.
+#include <stdio.h>
+
+int main()
+{
+    int length, breadth, area, perimeter;
+
+    scanf("%d %d", &length, &breadth);
+
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    printf("Area=%d, Perimeter=%d", area, perimeter);
+
+    return 0;
+}
